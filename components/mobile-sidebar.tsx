51b79679ad5a4cfb52a7cb049a57a4770c16e90b
Atlon1 <maciej.szajstek@gmail.com>
2024-01-14 19:56:58 +0100

    dashboard rdy

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -1,4 +1,6 @@
 "use client"
+
+import {useState, useEffect} from "react";
 import {Menu} from "lucide-react";
 
 import {Button} from "@/components/ui/button";
@@ -6,6 +8,17 @@ import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
 
 const MobileSidebar = () => {
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
 
     return (
         <Sheet>
