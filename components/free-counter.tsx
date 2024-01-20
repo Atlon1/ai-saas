dd0aad71e3a77fb21a635b3748b47df3e00bdeb8
Atlon1 <maciej.szajstek@gmail.com>
2024-01-20 21:17:58 +0100

    api limit component rdy

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -3,6 +3,9 @@
 import {useState, useEffect} from "react";
 import {Card, CardContent} from "@/components/ui/card";
 import {MAX_FREE_COUNTS} from "@/constants";
+import {Progress} from "@/components/ui/progress";
+import {Button} from "@/components/ui/button";
+import {Zap} from "lucide-react";
 
 interface SidebarProps {
     apiLimitCount: number
@@ -14,7 +17,7 @@ export const FreeCounter = ({apiLimitCount = 0}: SidebarProps) => {
 
     useEffect(() => {
         setMounted(true)
-    },[])
+    }, [])
 
     if (!mounted) return null
 
@@ -23,7 +26,20 @@ export const FreeCounter = ({apiLimitCount = 0}: SidebarProps) => {
             <Card className='bg-white/10 border-0'>
                 <CardContent className='py-6'>
                     <div className='text-center text-sm text-white mb-4 space-y-2'>
-                        <p className=''>{apiLimitCount} / {MAX_FREE_COUNTS} Free Generations</p>
+                        <p className=''>
+                            {apiLimitCount} / {MAX_FREE_COUNTS} Free Generations
+                        </p>
+                        <Progress className='h-3'
+                        value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
+                        >
+
+                        </Progress>
+                        <Button
+                            variant='premium'
+                            className='w-full'>
+                            Upgrade
+                            <Zap className='w-4 h-4 ml-2 fill-white'/>
+                        </Button>
                     </div>
                 </CardContent>
             </Card>
