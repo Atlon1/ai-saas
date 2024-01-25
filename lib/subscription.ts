1be2170fec58f11489f6a9acf85f088227b55bf9
Atlon1 <maciej.szajstek@gmail.com>
2024-01-25 20:51:08 +0100

    upload

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -1,1 +1,23 @@
 import {auth} from "@clerk/nextjs"
+
+import prismadb from "@/lib/prismadb";
+
+const DAY_IN_MS = 86_400_000;
+
+export const checkSubscription = async () => {
+    const {userId} = await auth()
+
+    if (!userId) {
+        return null
+    }
+
+    const subscription = await prismadb.userSubscription.findUnique({
+        where: {
+            userId
+        },
+        select: {
+            stripeSubscriptionId: true,
+        }
+    })
+
+}
