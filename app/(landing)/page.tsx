6d3cf61236e5dae91a369969b5e9edd5da180129
Atlon1 <maciej.szajstek@gmail.com>
2024-02-10 16:14:26 +0100

    added hero landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {LandingNavbar} from "@/components/landing-navbar";
+import {LandingHero} from "@/components/landing-hero";
 
 const LandingPage = () => {
     return (
