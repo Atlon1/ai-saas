5cead98283b8bcbf3e7d2d40f022016166b8fd5a
Atlon1 <maciej.szajstek@gmail.com>
2024-02-11 17:36:15 +0100

    Testimonials redy

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,10 +1,68 @@
 "use client";
 
+
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+
+const testimonials = [
+    {
+        name:"Maciej",
+        avatar: "A",
+        title: "Software Engineer",
+        description: "This is the best application I've used!"
+    }, {
+        name:"Maciej",
+        avatar: "A",
+        title: "Software Engineer",
+        description: "This is the best application I've used!"
+    }, {
+        name:"Maciej",
+        avatar: "A",
+        title: "Software Engineer",
+        description: "This is the best application I've used!"
+    }, {
+        name:"Maciej",
+        avatar: "A",
+        title: "Software Engineer",
+        description: "This is the best application I've used!"
+    }, {
+        name:"Maciej",
+        avatar: "A",
+        title: "Software Engineer",
+        description: "This is the best application I've used!"
+    },
+]
 export const LandingContent = () => {
 
     return (
-        <div>
-            Landing content
+        <div className='px-10 pb-20'>
+            <h2 className='text-center text-4xl text-white font-extrabold mb-10'>
+                Testimonials
+            </h2>
+            <div className='grid gird-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+                {testimonials.map((item) => {
+                    return (
+                        <Card
+                            className='bg-[#192339] border-none text-white'
+                            key={item.description}>
+                            <CardHeader>
+                                <CardTitle className='flex items-center gap-x-2'>
+                                    <div>
+                                        <p className='text-lg'>
+                                            {item.name}
+                                        </p>
+                                        <p className='text-zinc-400 text-sm'>
+                                            {item.title}
+                                        </p>
+                                    </div>
+                                </CardTitle>
+                                <CardContent className='pt-4 px-0'>
+                                    {item.description}
+                                </CardContent>
+                            </CardHeader>
+                        </Card>
+                    )
+                })}
+            </div>
         </div>
     )
 }
